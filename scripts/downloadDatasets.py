0a3383829d56f3ff9fd3ec4fe5ec51e3f6075aee
pab-h <dev.pab.2020@gmail.com>
2025-07-16 22:26:41 -0300

    feat: script para baixar os datasets

---
+++ scripts/downloadDatasets.py
@@ -0,0 +1,56 @@
+from os import path
+from os import remove
+from os import makedirs
+import zipfile
+import subprocess
+
+baseDir = path.abspath(
+    path.join(path.dirname(__file__), '../dataset')
+)
+
+makedirs(baseDir, exist_ok = True)
+
+datasets = [
+    {
+        "name": "prf",
+        "url":  "https://drive.google.com/uc?export=download&id=1-G3MdmHBt6CprDwcW99xxC4BZ2DU5ryR"
+    },
+    {
+        "name": "life-expectancy",
+        "url":  "https://www.kaggle.com/api/v1/datasets/download/kumarajarshi/life-expectancy-who"
+    }
+]
+
+def download(url, outputZipPath):
+    subprocess.run([
+        "curl", "-L", "-o", outputZipPath, url
+    ], check=True)
+
+def extractZip(zipPath, extractTo):
+    with zipfile.ZipFile(zipPath, 'r') as z:
+        z.extractall(extractTo)
+
+    remove(zipPath)
+
+for dataset in datasets:
+    
+    name      = dataset['name']
+    url       = dataset['url']
+    outputDir = path.join(baseDir, name)
+    
+    makedirs(outputDir, exist_ok=True)
+
+    zipPath = path.join(outputDir, f"{name}.zip")
+
+    print(f"Baixando: {name}")
+
+    try:
+        download(url, zipPath)
+        extractZip(zipPath, outputDir)
+        print(f"Concluído: {name}")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao baixar {name}: {e}")
+        
+    except zipfile.BadZipFile:
+        print(f"Arquivo ZIP inválido para {name}.")
